90bdf4b9ad4f3f2cdfa91ae5624eb67de62a444a
richard <richard_allcca_llano@hotmail.com>
2022-07-31 20:12:07 -0500

    themes sin barril index

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,13 @@ import type { AppProps } from 'next/app';
 import { NextUIProvider } from '@nextui-org/react';
 
 import '../styles/globals.css';
-// import { darkTheme } from '../themes/darktheme';
+import { darkTheme } from '../themes/darkTheme';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
+    <NextUIProvider theme={darkTheme}>
       <Component {...pageProps} />
-    // <NextUIProvider theme={}>
-    // </NextUIProvider>
+    </NextUIProvider>
   )
 }
 
