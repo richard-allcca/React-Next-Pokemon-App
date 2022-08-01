4d2c3a0dc911305a46607d8fb9317725878508c8
richard <richard_allcca_llano@hotmail.com>
2022-07-31 19:53:48 -0500

    delete DarkTheme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,13 @@ import type { AppProps } from 'next/app';
 import { NextUIProvider } from '@nextui-org/react';
 
 import '../styles/globals.css';
-import { darkTheme } from '../themes/darktheme';
+// import { darkTheme } from '../themes/darktheme';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <NextUIProvider theme={darkTheme}>
       <Component {...pageProps} />
-    </NextUIProvider>
+    // <NextUIProvider theme={}>
+    // </NextUIProvider>
   )
 }
 
