f688558b4dab4f61cb76661a21ed4b5472864ddd
richard-allcca <richard_allcca_llano@hotmail.com>
2023-06-10 10:46:35 -0500

    add meta SEO

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -7,6 +7,7 @@ interface Props extends PropsWithChildren {
   title?: string;
 }
 
+// Obtiene el dominio de origen para usarlo como path abosoluto de og:image
 const origin = (typeof window === 'undefined') ? '' : window.location.origin;
 
 export const Layout: FC<Props> = ({ children, title }) => {
@@ -17,11 +18,10 @@ export const Layout: FC<Props> = ({ children, title }) => {
         <meta name='author' content='Richard Allcca' />
         <meta name='description' content={`Información sobre el Pokemon ${title}`} />
         <meta name='keywords' content={`${title}. pokemon, pokedex`} />
-        <meta property="og:title" content={`Richard Allcca Llano`} />
+
+        <meta property="og:title" content={`Información sobre ${title}`} />
         <meta property="og:description" content={`Esta el la página sobre ${title}`} />
         <meta property="og:image" content={`${origin}/_next/image?url=%2Fimagenes%2Fbanner.png&w=640&q=75`} />
-        {/* <meta property="og:image" content="http://localhost:3000/_next/image?url=%2Fimagenes%2Fbanner.png&w=640&q=75" /> */}
-        {/* <meta property="og:image" content="https://res.cloudinary.com/thouma/image/upload/v1659297257/banner_rrvbdo.png" />  */}
       </Head>
 
       <Navbar />
